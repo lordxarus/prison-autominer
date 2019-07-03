ed8b10d63251ceaff2b15df22adf167a5f967660
Jeremy Ball <lordxarus@gmail.com>
2019-07-02 20:17:41 -0400

    Added left click to despawn and right click to open menu

--- src/main/kotlin/com/lordxarus/autominer/npc/AutominerTrait.kt
+++ src/main/kotlin/com/lordxarus/autominer/npc/AutominerTrait.kt
@@ -1,9 +1,14 @@
 package com.lordxarus.autominer.npc
 
+import com.lordxarus.autominer.util.getModel
+import com.lordxarus.autominer.util.plugin
+import net.citizensnpcs.api.event.NPCLeftClickEvent
+import net.citizensnpcs.api.event.NPCRightClickEvent
 import net.citizensnpcs.api.trait.Trait
+import org.bukkit.event.EventHandler
 
 
-class AutominerTrait: Trait("automine") {
+class AutominerTrait : Trait("automine") {
 
     val autominerModel by lazy { AutominerModel(npc) }
 
@@ -22,5 +27,22 @@ class AutominerTrait: Trait("automine") {
         }
     }
 
+    @EventHandler
+    fun onLeftClick(e: NPCLeftClickEvent) {
+        if (e.npc.isSpawned) {
+            if (e.npc == npc && e.clicker.player == autominerModel.player) {
+                getModel(e.clicker.player).npc.despawn()
+            }
+        }
+    }
+
+    @EventHandler
+    fun onRightClick(e: NPCRightClickEvent) {
+        if (e.npc.isSpawned) {
+            if (e.npc == npc && e.clicker.player == autominerModel.player) {
+                plugin.inventory.open(e.clicker)
+            }
+        }
+    }
 }
 
